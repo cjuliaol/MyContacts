82a96b8d8022ed2326278429d559d2c452e0be59
cjuliaol <cjuliaol@gmail.com>
2015-07-01 08:19:41 -0400

    First commit

---
+++ app/src/main/java/com/example/thewizard/cjuliaol/mycontacts/Contact.java
@@ -0,0 +1,17 @@
+package com.example.thewizard.cjuliaol.mycontacts;
+
+/**
+ * Created by cjuliaol on 30/06/2015.
+ */
+public class Contact {
+
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
---
+++ app/src/main/java/com/example/thewizard/cjuliaol/mycontacts/ContactListActivity.java
@@ -0,0 +1,99 @@
+package com.example.thewizard.cjuliaol.mycontacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ContactListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+
+        for (int i = 0; i < 25; i++) {
+            Contact contact1 = new Contact();
+            contact1.setName("John Smith");
+            contacts.add(contact1);
+        }
+
+        ListView listView = (ListView) findViewById(R.id.contact_list_view);
+        listView.setAdapter(new ContactAdapter(contacts));
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            int previousFirstItem;
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                // To Hide ActionBar when you scroll down
+                if (firstVisibleItem > previousFirstItem) {
+                    getSupportActionBar().hide();
+                } else if (firstVisibleItem < previousFirstItem) {
+                    getSupportActionBar().show();
+                }
+
+                previousFirstItem = firstVisibleItem;
+            }
+        });
+
+    }
+
+
+    private class ContactAdapter extends ArrayAdapter<Contact> {
+
+        ContactAdapter(ArrayList<Contact> contacts) {
+            super(ContactListActivity.this, R.layout.contact_list_row, R.id.contact_row_name, contacts);
+        }
+
+        // To customize the custon row
+        // To show what you want to show
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+            Contact contact = getItem(position);
+            TextView nameTextView = (TextView) convertView.findViewById(R.id.contact_row_name);
+            nameTextView.setText(contact.getName());
+
+            return convertView;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_contact_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
